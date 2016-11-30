7ac121cf97f4ad5c7e1b1894acd6e21143c228cd
tjking20 <tjking20@outlook.com>
2016-11-30 18:35:56 -0500

    game is fully functional

--- assets/script.js
+++ assets/script.js
@@ -26,62 +26,61 @@ $(document).ready(function() {
 	console.log("huzzah!");
 
 
-// 1. create variables for targetNumber, score, buttons 1-4,wins, losses 
-var options = ["39","40","41","42", "43", "44", "45"];
-var targetNumber = "";
 var score = 0;
 var wins = 0;
 var losses = 0;
+var targetNumber = '';
+var value = '';
 
 
 
-// Computer selects a random number for 
+// Computer selects a random target number adn values for the buttons for----------- 
+function reset(){
+	function randomOption(){
+		targetNumber = Math.floor(Math.random() * (120-19)+19);
+		$("#targetNumber").html(targetNumber);
+	}
 
-targetNumber = options[Math.floor(Math.random() * options.length)];
-$("#targetNumber").html(targetNumber)
+	function randomVal(btn){
+		value = Math.floor(Math.random() * (10 - 1) + 1);
+		$(btn).val(value);
+	}
+	randomOption();
+	randomVal("#button1");
+	randomVal("#button2");
+	randomVal("#button3");
+	randomVal("#button4");
+	score=0
+	$("h3").html("0");
+
+}
+reset();
 
 
 
-// 3. create a pointsAdded function that can work for all 4 crystals. 
-// pass in the relevant button info to make the function work.
-
-// -create an onclick function using jquery. the function will add the 
-// "crystal value(reference data, including the calculator game code)" 
-// to score (score+=crystalValue)
-
-// function buttonClick(button){
 	$(document).on("click", ".btn", function(){
-		score = score + parseInt($(this).attr("value"));
+		score += parseInt($(this).attr("value"));
 		$("h3").html(score);
 
+		// Conditional increments wins and losses, and reset values
+
 		if(score == targetNumber){
-			alert("winner!");
+			wins++;
+			$("#wins").html("Wins: " + wins);
+			reset();
+
+
 
 		}else if (score > targetNumber){
-			alert("loser!");
+			losses++;
+			$("#losses").html("Losses: " + losses);
+			reset();
 		}
 
 
 
 	
 	});
-
-// };
-
-
-
-
-
-
-
-
-// 4. add conditional if TargetNumber == score alert(winner)
-// else if score > targeNumber then alert (loser) update wins or losses respectively.
-
-
-// 5. also need to reassign values to the crystals
-// - to reassign button value create a funtion using jQuery that reassigns value, 
-// and call the functions in the reset.
 
 
 
